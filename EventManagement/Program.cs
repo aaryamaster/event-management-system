8e126675abb40cefb51d58d15e0d0368091cf22b
aaryamaster <75297750+aaryamaster@users.noreply.github.com>
2024-07-30 18:46:42 -0400

    Update Program.cs

--- EventManagement/Program.cs
+++ EventManagement/Program.cs
@@ -61,7 +61,7 @@ app.MapControllerRoute(
 
 app.MapRazorPages();
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "80";
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 app.Urls.Add($"http://*:{port}");
 
 app.Run();
